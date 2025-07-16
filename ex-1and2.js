66a68e87cff8bd39ad218fc1e557e9302bf67d33
Anuna <anuaq20@gmail.com>
2025-07-16 13:32:53 +0700

    feat(ex-1and2): add variable creation and reassignment for exercises 1 and 2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
-//Exercise #1: Variable Creation
-//Start coding here
+// Exercise #1: Variable Creation
+let firstname = "Karter"
+let lastname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurantname = "Chef's Table by Chef James"
+let  totalbill = "15000"
+let totalrestaurantopen = "1500"
+
+// Exercise #2: Variable Creation
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalbill = 5000;
+totalrestaurantopen = 2500;
