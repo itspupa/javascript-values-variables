233d240350e83a3d1138d59197277518288689ed
Anuna <anuaq20@gmail.com>
2025-07-16 13:51:35 +0700

    feat(ex-1and2, ex-3): log variables and calculate fahrenheit from celsius

--- ex-1and2.js
+++ ex-1and2.js
@@ -6,6 +6,7 @@ let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
 const restaurantname = "Chef's Table by Chef James"
 let  totalbill = "15000"
 let totalrestaurantopen = "1500"
+console.log(firstname, lastname, address );
 
 // Exercise #2: Variable Creation
 address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
