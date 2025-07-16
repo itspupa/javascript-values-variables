3470c08707fcdc0161f2dad6f69fc23f9adca84a
Phumphut <phumphut.rueng@gmail.com>
2025-07-16 13:34:50 +0700

    feat: javascript ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,18 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+console.log(firstName, lastName, address);
+
+const restaurantName = "Chef's Table by Chef James";
+const price = 15000;
+const openRestaurantNumber = 1500;
+console.log(restaurantName, price, openRestaurantNumber);
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+price = 5000;
+openRestaurantNumber = 2500;
+console.log(address);
